54834bd4227eb7606136655728d64eec943a414f
Mark Hollomon <mhhollomon@gmail.com>
2022-05-02 20:03:21 -0400

    Fix up handling of null terminators in OSC strings.
    This fixes #1

--- src/lib/osc.cpp
+++ src/lib/osc.cpp
@@ -2,6 +2,18 @@
 #include <iostream>
 #include <string>
 
+std::string NULL_CHAR{ "\0", 1 };
+
+std::string pad_string(std::string padee) {
+	auto padding_size = padee.size() % 4;
+	for (; padding_size > 0; --padding_size) {
+		padee.append(NULL_CHAR);
+	}
+
+	return padee;
+
+}
+
 std::string OSCMessage::method_name() {
 	auto pos = addr_.rfind('/');
 	return addr_.substr(pos + 1);
@@ -12,14 +24,10 @@ std::string OSCMessage::to_data() {
 
 	std::string retval = addr_;
 
-	auto padding_size = addr_.size() % 4;
+	// OSC string are null terminated. This needs to be done BEFORE the padding.
+	retval += NULL_CHAR;
 
-	std::string null_char{ "\0", 1 };
-	for (; padding_size > 0; --padding_size) {
-		std::cout << "Appending null";
-		retval.append(null_char);
-	}
-	retval.append(",");
+	retval = pad_string(retval) + pad_string(",");
 
 	return retval;
 }
@@ -27,21 +35,15 @@ std::string OSCMessage::to_data() {
 OSCMessage OSCMessage::from_data(std::string_view data) {
 	// note : currently only handles datagrams (i.e. no starting size and no SLIP)
 	// per examples at https://opensoundcontrol.stanford.edu/spec-1_0-examples.html
-	// The address pattern is also padded to a 4 bit boundary.
+	// The address pattern is also padded to a 4 byte boundary.
 
 	OSCMessage msg;
 
-	// We are going to assume that it includes a type tag.
-	auto type_tag_pos = data.find(',');
-	std::cout << "tag pos = " << type_tag_pos << "\n";
-	auto new_addr = data.substr(0, type_tag_pos);
-	auto pos = data.find('\0');
-	std::cout << "pos = " << pos << "\n";
+	// There will always be a null at the end of the path. Find it.
+	auto pos = data.find(NULL_CHAR);
+	std::cout << "addr ends at pos = " << pos << "\n";
 
-	if (pos != std::string::npos) {
-		std::cout << "Stripping nulls\n";
-		new_addr = new_addr.substr(0, pos);
-	}
+	auto new_addr = data.substr(0, pos);
 
 	// force the copy of the underlying bits.
 	msg.addr_ = std::string(new_addr);
