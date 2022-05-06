5ca65c1851a26556ecb19bb38dfe959c7aa51d14
Mark Hollomon <mhhollomon@gmail.com>
2022-05-06 15:44:10 -0400

    Remove unused file.

--- src/include/application_controller_base.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class ApplicationControllerBase {
-public:
-	virtual void launch_app(std::string path_or_name) = 0;
-	virtual void launch_app(std::string path_or_name, std::string arg) = 0;
-
-	virtual ~ApplicationControllerBase() = default;
-};
